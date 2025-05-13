20556af41ef5840d7fa96d65d44dbe77788302ff
Albe Marini-Audouard <duck.marini@gmail.com>
2025-05-14 01:35:44 +0200

    💫 poses ready for dwarf Muscle

--- main/visual_entity_component_handler.gd
+++ main/visual_entity_component_handler.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var sprites_parent: Node2D = $Cornu
+@onready var sprites_parent: Node2D = $Muscle
 @onready var skeleton:= $Skeleton2D
 @onready var animator: AnimationPlayer = $Animator
 var weapon_node: Sprite2D
